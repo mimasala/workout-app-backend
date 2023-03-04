ca008ca556951eba8e5e01922a7898efe2811006
mimasala <65661176+mimasala@users.noreply.github.com>
2023-03-04 18:30:51 +0100

    add project

---
+++ src/main/java/nft/traderplace/WorkoutAppBackendApplication.java
@@ -0,0 +1,13 @@
+package nft.traderplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nft/traderplace/WorkoutAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nft.traderplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
